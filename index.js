f9475d197b359f2da096c2f4028caf4f2be897cb
siddikur-dev <siddikur.dev@gmail.com>
2025-12-10 21:57:53 +0600

    some

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config(); 
 const app = express();
 const port = process.env.PORT || 3000;
 const admin = require("firebase-admin");
